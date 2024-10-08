e519616b95339f921edd0e3b11c89748b1ed1e2a
Matvey Gotfrid <matveygot@yandex.ru>
2024-10-08 23:20:12 +0300

    Add GET and POST handlers

--- server/POSThandler.go
+++ server/POSThandler.go
@@ -3,39 +3,60 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func POSThandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		var newUser UserData
-		decoder := json.NewDecoder(r.Body)
-		defer r.Body.Close()
+		var request struct {
+			Action string   `json:"action"`
+			User   UserData `json:"user"`
+		}
+
+		if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+			http.Error(w, "Ошибка декодирования JSON", http.StatusBadRequest)
+			return
+		}
 
 		dataMutex.Lock()
 		defer dataMutex.Unlock()
 
-		if err := decoder.Decode(&newUser); err != nil {
-			http.Error(w, "Ошибка декодирования JSON", http.StatusBadRequest)
-			return
-		}
 		data, err := loadUserDataFromFile()
 		if err != nil {
-			log.Println("Ошибка загрузки базы данных")
+			http.Error(w, "Ошибка загрузки базы данных", http.StatusInternalServerError)
+			return
 		}
 
-		data[newUser.Login] = newUser
-		err = saveUserDataToFile(data)
+		switch request.Action {
+		case "register":
+			if _, exists := data[request.User.Login]; exists {
+				http.Error(w, "Пользователь уже существует", http.StatusConflict)
+				return
+			}
+			data[request.User.Login] = request.User
+			if err := saveUserDataToFile(data); err != nil {
+				http.Error(w, "Ошибка сохранения записи в базу данных", http.StatusInternalServerError)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+			fmt.Printf("Пользователь %s успешно добавлен\n", request.User.Login)
 
-		if err != nil {
-			log.Println("Ошибка сохранения записи в базу данных")
+		case "login":
+			existingUser, exist := data[request.User.Login]
+			if !exist {
+				http.Error(w, "Пользователь не найден", http.StatusUnauthorized)
+				return
+			}
+			if existingUser.Password != request.User.Password {
+				http.Error(w, "Неверный пароль", http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
+
+		default:
+			http.Error(w, "Некорректное действие", http.StatusBadRequest)
 		}
-
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "Пользователь %s успешно добавлен", newUser.Login)
 		return
 	}
-
-	http.Error(w, "Метод не разрешён", http.StatusMethodNotAllowed)
+	http.Error(w, "Метод не разрешен", http.StatusMethodNotAllowed)
 }
--- server/startHTTPserver.go
+++ server/startHTTPserver.go
@@ -8,7 +8,8 @@ import (
 
 func startHTTPServer(address string) {
 	http.Handle("/", http.FileServer(http.Dir("static")))
-	http.HandleFunc("/userdata.json", POSThandler)
+	http.HandleFunc("/request", POSThandler)
+	http.HandleFunc("/loadData", GEThandler)
 	fmt.Printf("HTTP-сервер открыт и прослушивает адрес %s\n", address)
 	if err := http.ListenAndServe(address, nil); err != nil {
 		log.Fatalln("Ошибка запуска HTTP-сервера", err)
